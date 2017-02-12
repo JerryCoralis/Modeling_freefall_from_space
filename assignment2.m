16270ab5e7203c82a0c31711706deddbda00a1f7
Jerry Li <jerry.coralis@gmail.com>
2017-02-11 17:37:15 -0800

    Add files via upload
    Crossroad: which import function to use.

---
+++ assignment2.m
@@ -0,0 +1,13 @@
+function output = assignment2(arg1,arg2,arg3)
+
+filename = 'RedBullStratosData180.xlsx';
+% [num] = xlsread(filename)
+% readtable(filename)
+% importdata(filename)
+
+% disp(num)
+
+% abvSea = 38969.4;
+disp('buffer')
+end 
+
